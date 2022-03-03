e56c7af6f27f15ec25d42315106ae2ecb63e1b1d
Jacob Reese <jacobreese123@gmail.com>
2022-03-02 20:58:36 -0500

    create initial SearchBar, CurrentWeather, and WeeklyForecast components

---
+++ weather-web-app/src/components/CurrentWeather.js
@@ -0,0 +1,21 @@
+const CurrentWeather = () => {
+  return (
+    <div>
+      <h1>Current Weather</h1>
+      <ul>
+        <li>Temp</li>
+        <li>Hi-Lo temp for day</li>
+        <li>Feels like</li>
+        <li>Current Time with display options (ex. moon at night, clouds)</li>
+        <li>Precipitation</li>
+        <li>Wind Speed</li>
+        <li>Sunrise/Sunset</li>
+        <li>Humidity</li>
+        <li>UV index</li>
+        <li>Pollen</li>
+      </ul>
+    </div>
+  );
+};
+
+export default CurrentWeather;
---
+++ weather-web-app/src/components/WeeklyForecast.js
@@ -0,0 +1,10 @@
+const WeeklyForecast = () => {
+    return (
+        <div>
+        <h1>7 Day Forecast</h1>
+      </div>
+    );
+  };
+  
+  export default WeeklyForecast;
+  

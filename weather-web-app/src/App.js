603b4d56b6f3147acb9c043299bc4888734e4ca8
Jacob Reese <jacobreese123@gmail.com>
2022-03-05 16:33:05 -0500

    add initialy location check to show weather in current location

--- weather-web-app/src/App.js
+++ weather-web-app/src/App.js
@@ -2,12 +2,36 @@ import "./App.css";
 import CurrentWeather from "./components/CurrentWeather";
 import SearchBar from "./components/SearchBar";
 import WeeklyForecast from "./components/WeeklyForecast";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [longitude, setLongitude] = useState();
+  const [latitude, setLatitude] = useState();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+      });
+
+      await fetch(
+        `https://api.openweathermap.org/data/2.5/weather/?lat=${latitude}&lon=${longitude}&units=imperial&appid=1e39a7bcb850e84609518a1b0086e36e`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+          console.log(result);
+        });
+    };
+    fetchData();
+  }, [latitude, longitude]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <SearchBar />
+        <SearchBar input={location} />
         <CurrentWeather />
         <WeeklyForecast />
       </header>
